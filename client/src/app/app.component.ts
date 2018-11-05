873816266cb14859b2c393e14d4d6ef5b950af74
Zinzidane <ya-gans@mail.ru>
2018-11-05 18:45:21 +0300

    reinstall project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { TokenService } from './services/token.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router, private tokenService: TokenService) {}
-
-  ngOnInit() {
-    const token = this.tokenService.GetToken();
-
-    if(token) {
-      this.router.navigate(['streams']);
-    } else {
-      this.router.navigate(['/']);
-    }
-  }
+export class AppComponent {
+  title = 'client';
 }
